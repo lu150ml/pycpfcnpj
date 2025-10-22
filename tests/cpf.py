7173245442df591eb79f186dce82d249b7bab18c
lu150ml <luishenriqueribeiro3@gmail.com>
2025-10-22 12:04:28 -0300

    criado função que valida listas de cpf

--- tests/cpf.py
+++ tests/cpf.py
@@ -1,14 +1,15 @@
 from pycpfcnpj import cpfcnpj
 
-cpf_list= []
-qtd = int(input("Quantos CPFs/CNPJs deseja validar? "))
-for i in range(qtd):
-    cpf_list.append(input("Digite o CPF/CNPJ: "))
+
+entrada = input("Cole a lista de CPFs separados por vírgula:\n> ")
+
+
+cpf_list = [cpf.strip() for cpf in entrada.split(",")]
 
 print("\n📋 Resultados da validação:\n")
+
+
 for cpf in cpf_list:
     valido = cpfcnpj.validate(cpf)
-    if valido:
-        print(f"{cpf} ✅ Válido")
-    else:
-        print(f"{cpf} ❌ Inválido")
+    status = "✅ Válido" if valido else "❌ Inválido"
+    print(f"{cpf} → {status}")
